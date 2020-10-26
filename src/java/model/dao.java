9623451c9a37f0a83e53b2dcc8f5bcdf72c7533a
SeanAles1 <672018112@student.uksw.edu>
2020-10-27 00:20:06 +0700

    Add retrieve product

--- src/java/model/dao.java
+++ src/java/model/dao.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author COSTUMER
- */
+import controller.NewHibernateUtil;
+import controller.Product;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class dao {
+    public List<Product> retriveProduct(){
+        List prod = new ArrayList();
+        Product prod1=new Product();
+        Transaction trans=null;
+        Session session=NewHibernateUtil.getSessionFactory().openSession();
+        try
+        {
+            trans=session.beginTransaction();
+            Query query=session.createQuery("from Product");
+            prod=query.list();
+            trans.commit();   
+        }
+        catch(Exception e)
+        {
+            
+        }
+        return prod;
+    }
+
+    public void addProduct(Product product){
+        Transaction trans=null;
+        Session session=NewHibernateUtil.getSessionFactory().openSession();
+        try 
+        {
+            trans=session.beginTransaction();
+            session.save(product);
+            trans.commit();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    
     
 }
