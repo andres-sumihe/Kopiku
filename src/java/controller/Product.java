fbbac406818f3b8d702d8db0aa3c514de6ccef73
SeanAles1 <672018112@student.uksw.edu>
2020-10-27 23:02:43 +0700

    Add Some Method and Variable for Update Product

--- src/java/controller/Product.java
+++ src/java/controller/Product.java
@@ -14,6 +14,10 @@ public class Product  implements java.io.Serializable {
      private String description;
      private float price;
      private String path;
+     public String nameUpdate;
+     public String descriptionUpdate;
+     public String pathUpdate;
+     public float priceUpdate;
 
     public Product() {
     }
@@ -35,6 +39,22 @@ public class Product  implements java.io.Serializable {
     }
     public String getName() {
         return this.name;
+    }
+    
+    public String getNameUpdate() {
+        return this.nameUpdate;
+    }
+    
+    public String getPathUpdate() {
+        return this.pathUpdate;
+    }
+    
+    public float getPrice() {
+        return this.priceUpdate;
+    }
+    
+    public String getDescriptionUpdate() {
+        return this.descriptionUpdate;
     }
     
     public void setName(String name) {
@@ -87,20 +107,23 @@ public class Product  implements java.io.Serializable {
     
     public void update(){
         dao pdao=new dao();
+        this.name = nameUpdate;
+        this.price = priceUpdate;
+        this.description = descriptionUpdate;
+        this.path = pathUpdate;
         pdao.updateProduct(this);
     }
     
     public List<Product> getbyid(){ 
         dao pdao=new dao();
         List<Product> prod=pdao.getbyID(id);
-        name=prod.get(0).name;
-        description=prod.get(0).description;
-        path=prod.get(0).path;
-        price=prod.get(0).price;
+        nameUpdate=prod.get(0).name;
+        descriptionUpdate=prod.get(0).description;
+        pathUpdate=prod.get(0).path;
+        priceUpdate=prod.get(0).price;
         return prod;
     }
-    
-    
+      
 }
 
 
